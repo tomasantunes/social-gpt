2f737a66be28ff22007188bd54ccd54f3c49bcd9
Tomás Antunes <tomasantunes@gmail.com>
2023-04-20 15:35:47 +0100

    Fix error on bots initialization.

--- app.js
+++ app.js
@@ -124,13 +124,12 @@ var bots = [
   }
 ];
 
-function initBots() {
+async function initBots() {
   for (var i in bots) {
     var bot = bots[i];
     if (bot.dialogue.length > 0) {
-      getAnswer(bot.dialogue, function(messages) {
-        bot.dialogue = messages;
-      });
+      var messages = await getAnswer(bot.dialogue);
+      bot.dialogue = messages;
     }
   }
 }
